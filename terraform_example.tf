188f7947946b329526bae6308b4194d08b2b1be7
pramodk05 <pramodk05@gmail.com>
2019-08-24 13:17:13 +0530

    key update

--- terraform_example.tf
+++ terraform_example.tf
@@ -1,7 +1,7 @@
 provider "aws" {
     region = "us-east-1"
-    access_key = "AKIATOLN4SYY5NVPCTEP"
-    secret_key = "ju2RAR2DtdQSMDcdNsuDZZC73JVH52qqfcEcUyMG"
+    access_key = "AWS_ACCESS_KEY"
+    secret_key = "AWS_SECRET_KEY"
 }
 
 # Creaing EC2 instance
