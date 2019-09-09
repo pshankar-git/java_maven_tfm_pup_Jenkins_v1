4e7ca3a9ef05321626051f00a85c3db9b82a3569
root <root@ip-172-31-88-195.ec2.internal>
2019-09-09 16:48:48 +0000

    .

--- tc_pup_agent_setup.sh
+++ tc_pup_agent_setup.sh
@@ -1,28 +1,39 @@
-tc_server_pri_dns=`sed -n '1p' < tc_pri_dns.txt`
-echo $tc_server_pri_dns
-tc_server_pri_ip=`sed -n '1p' < tc_pri_ip.txt`
-echo $tc_server_pri_ip
-pupmaster_pri_ip=`sed -n '1p' < pup_master_pri_ip.txt`
-echo $pupmaster_pri_ip
-pupmaster_pri_dns=`sed -n '1p' < pup_master_pri_dns.txt`
-echo $pupmaster_pri_dns
+#!/bin/bash
+#
+# This script does the following
+#
+#    * Create AWS resources like EC2 instance, Security Group and a Key pair for Tomcat App Server
+#    * Install Puppet Agent package on the newly create Tomcat ec2 instance
+#    * Configure /etc/puppet/puppet.conf for thr Tomcat Puppet Agent
+#    * Generate certificate signing request to Puppet Master
+#    * Puppet Master to sign the CSR generated and add Tomcat server as a puppet agent
+
+# FETCHING TOMCAT AND PUPPET MASTER SERVER DNS and IP's
+
+printf "\nFETCHING TOMCAT AND PUPPET MASTER SERVER DNS and IP's\n"
+TC_SERVER_PRI_DNS=`sed -n '1p' < tc_pri_dns.txt`
+echo $TC_SERVER_PRI_DNS
+TC_SERVER_PRI_IP=`sed -n '1p' < tc_pri_ip.txt`
+echo $TC_SERVER_PRI_IP
+PUPMASTER_PRI_IP=`sed -n '1p' < pup_master_pri_ip.txt`
+echo $PUPMASTER_PRI_IP
+PUPMASTER_PRI_DNS=`sed -n '1p' < pup_master_pri_dns.txt`
+echo $PUPMASTER_PRI_DNS
+
+printf "\nSSH TO TOMCAT EC2 INSTANCE AND INSTALL PUPPET AGENT\n"
 sleep 20
-#ssh -i tomcat_ec2_key -tt ubuntu@$tc_server_pri_dns -oStrictHostKeyChecking=no <<EOF
-ssh -t ubuntu@$tc_server_pri_dns -i "tomcat_ec2_key" -oStrictHostKeyChecking=no "/usr/bin/sudo bash -c 'hostname tomcatpuppetagent.ec2.internal; \
+ssh -t ubuntu@$TC_SERVER_PRI_DNS -i "tomcat_ec2_key" -oStrictHostKeyChecking=no "/usr/bin/sudo bash -c 'hostname tomcatpuppetagent.ec2.internal; \
 echo tomcatpuppetagent.ec2.internal > /etc/hostname; \
-echo $pupmaster_pri_ip puppetmaster.ec2.internal ${pupmaster_pri_dns} >> /etc/hosts; \
-echo ${tc_server_pri_ip} tomcatpuppetagent.ec2.internal ${tc_server_pri_dns} >> /etc/hosts; \
+echo $PUPMASTER_PRI_IP puppetmaster.ec2.internal ${PUPMASTER_PRI_DNS} >> /etc/hosts; \
+echo ${TC_SERVER_PRI_IP} tomcatpuppetagent.ec2.internal ${TC_SERVER_PRI_DNS} >> /etc/hosts; \
 wget https://apt.puppetlabs.com/puppet-release-bionic.deb; \
 dpkg -i puppet-release-bionic.deb; \
 apt-get update -y; \
 apt-get install puppet -y; \
 exit;'"
-#EOF
 
-
-echo "Configuring puppet agent on Tomcat"
-#ssh -i tomcat_ec2_key -tt ubuntu@$tc_server_pri_dns -oStrictHostKeyChecking=no <<EOF
-ssh -t ubuntu@$tc_server_pri_dns -i "tomcat_ec2_key" -oStrictHostKeyChecking=no "/usr/bin/sudo bash -c 'mv /etc/puppet/puppet.conf /etc/puppet/puppet.conf.orig; \
+printf "\nCONFIGURING PUPPET AGENT ON TOMCAT EC2 INSTANCE\n"
+ssh -t ubuntu@$TC_SERVER_PRI_DNS -i "tomcat_ec2_key" -oStrictHostKeyChecking=no "/usr/bin/sudo bash -c 'mv /etc/puppet/puppet.conf /etc/puppet/puppet.conf.orig; \
 echo [main] > /etc/puppet/puppet.conf; \
 echo ssldir = /var/lib/puppet/ssl >> /etc/puppet/puppet.conf; \
 echo certname = tomcatpuppetagent.ec2.internal >> /etc/puppet/puppet.conf; \
@@ -32,17 +43,14 @@ export PATH=$PATH:/opt/puppetlabs/puppet/bin; \
 systemctl restart puppet; \
 systemctl enable puppet; \
 exit;'"
-#EOF
 
-echo "Signing puppet certs from Puppet master"
-ssh -i /opt/pup_setup_tf/puppet_ec2_key -t ubuntu@$pupmaster_pri_ip -oStrictHostKeyChecking=no "/usr/bin/sudo bash -c 'echo ${tc_server_pri_ip} tomcatpuppetagent.ec2.internal ${tc_server_pri_dns} >> /etc/hosts; \
+printf "\nSIGNING PUPPET CERTS FROM PUPPET MASTER FOR NEWLY CREATED TOMCAT PUPPET AGENT\n"
+ssh -i /opt/pup_setup_tf/puppet_ec2_key -t ubuntu@$PUPMASTER_PRI_IP -oStrictHostKeyChecking=no "/usr/bin/sudo bash -c 'echo ${TC_SERVER_PRI_IP} tomcatpuppetagent.ec2.internal ${TC_SERVER_PRI_DNS} >> /etc/hosts; \
 puppet cert list; \
 puppet cert sign tomcatpuppetagent.ec2.internal; \
 exit;'"
-#EOF
 
-echo "Test puppet agent (Tomcat server) connection"
-ssh -i tomcat_ec2_key -t ubuntu@$tc_server_pri_dns -oStrictHostKeyChecking=no "/usr/bin/sudo bash -c 'puppet agent --test; \
+printf "\nTEST TOMCAT PUPPET AGENT CONNECTION TO PUPPET MASTER AND APPLY CATALOG\n"
+ssh -i tomcat_ec2_key -t ubuntu@$TC_SERVER_PRI_DNS -oStrictHostKeyChecking=no "/usr/bin/sudo bash -c 'puppet agent --test; \
 exit;'"
-#EOF
 
